f9a06cc0c0312990de3d6789f4611d569ee7f3e2
Joseph Houser <josephghouser@gmail.com>
2020-11-10 22:57:35 -0600

    worked through most of the databinding examples

--- databinding/src/app/app.component.ts
+++ databinding/src/app/app.component.ts
@@ -3,12 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username: string = ""
+
+  allowUsernameButtonClick = false;
+
+  onUpdateUserName(event){
+    this.username = event.target.value
+    if (this.username != ""){
+      this.allowUsernameButtonClick = true;
+    }
+  }
+
+  onClearUsername(){
+    this.username = "";
+  }
+  getUsername(){
+    return this.username;
+  }
 }
