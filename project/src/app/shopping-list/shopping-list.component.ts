04e8b42a47e8b1be7d5d007974020efe008dd040
Joseph Houser <josephghouser@gmail.com>
2020-12-10 22:38:55 -0600

    added shopping list initial logic

--- project/src/app/shopping-list/shopping-list.component.ts
+++ project/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('00 flour', 1),
+    new Ingredient('san marzano tomatoes', 3)
+  ];
   constructor() { }
 
   ngOnInit(): void {
